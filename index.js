b1a67cef437c386ee06f2fac82845d33ac27912c
saha566789 <tomasaha3456@gmail.com>
2023-11-06 02:58:09 +0600

    bookCollection added

--- index.js
+++ index.js
@@ -25,13 +25,39 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+
+    const bookCollection = client.db('BookDB').collection('library')
+    
+
+    app.get('/libraries/:category_name', async (req, res)=>{
+        const category_name= req.params.category_name;
+       
+        const result = await bookCollection.find({ 'Category': category_name }).toArray();
+        res.send(result);
+    })
+
+
+    app.get('/libraries',async(req,res)=>{
+        const cursor = bookCollection.find()
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.post('/libraries', async (req, res) => {
+        const newProduct = req.body;
+        console.log(newProduct);
+        const result = await bookCollection.insertOne(newProduct);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
